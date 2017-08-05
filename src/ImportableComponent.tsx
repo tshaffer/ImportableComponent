6eac989a7837c2f5eaebf16564451f735ef59be2
Ted Shaffer <ted@brightsign.biz>
2017-08-05 12:16:09 -0700

    updated the component to display the properties as spans on different lines.

--- src/ImportableComponent.tsx
+++ src/ImportableComponent.tsx
@@ -1,19 +1,41 @@
 import * as React from 'react';
 
 export interface icProps {
-  prop1 : string;
-  prop2 : number;
+  label1 : string;
+  label2 : string;
 }
 
 export default class ImportableComponent extends React.Component<icProps, object> {
+
+  constructor(props : any) {
+    super(props);
+    console.log(props);
+  }
 
   render() {
 
     console.log('ImportableComponent:: RENDER INVOKED');
 
+    console.log(this.props);
+
+    if (!this.props.label1 || this.props.label1 === '') {
+      return null;
+    }
+
+    if (!this.props.label2 || this.props.label2 === '') {
+      return null;
+    }
+
     return (
       <div>
-        <div>Eat Pizza Every Day!</div>
+        <div>
+          Eat Pizza Every Day!
+          <br/>
+          <span>{this.props.label1}</span>
+          <br/>
+          <span>{this.props.label2}</span>
+          <br/>
+        </div>
       </div>
     );
   }
